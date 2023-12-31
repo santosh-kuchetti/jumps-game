db4d942b0ec584f4acb3d007b45c3de99e318911
santosh-kuchetti <kuchettisantosh@gmail.com>
2023-12-31 16:47:36 +0530

    pakge name change

--- game/ios/jumpyBumpyTests/jumpyBumpyTests.m
+++ game/ios/jumpyBumpyTests/jumpyBumpyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface jumpyBumpyTests : XCTestCase
+@interface jumpybumpyTests : XCTestCase
 
 @end
 
-@implementation jumpyBumpyTests
+@implementation jumpybumpyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
